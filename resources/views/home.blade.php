a38b3a14cbb91a374b9d6ba63d97bfe878a4ca2f
Chandrakant Patel <cto@rewathi.com>
2026-02-20 18:24:11 +0530

    Slider  Autoplay

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -954,7 +954,7 @@
                     1024: { slidesPerView: 4, spaceBetween: 0 },
                     1200: { slidesPerView: 5, spaceBetween: 0 },
                     1400: { slidesPerView: 6, spaceBetween: 0 },
-                    1600: { slidesPerView: 7, spaceBetween: 0 },
+                    //1600: { slidesPerView: 7, spaceBetween: 0 },
                 },
             });
 
